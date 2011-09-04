6b4a4391ae1695164b5807a0b8015aa72a5eb7d4
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-03 23:20:40 -0500

    0.0.1

---
+++ src/clj_librato/metrics.clj
@@ -0,0 +1,11 @@
+(ns clj-librato.metrics
+	(:require [clj-json.core :as json]
+		      [clj-http.client :as client]))
+
+(defn collate [user api-key gauges counters]
+	(client/post "https://metrics-api.librato.com/v1/metrics.json"
+		{:basic-auth [user api-key]
+		 :body (json/generate-string {:gauges gauges :counters counters})
+		 :content-type :json
+		 :accept :json
+		 :throw-exceptions false}))
