273a7247e5069471100a9f928b171cedb78ce9d5
Aphyr <aphyr@aphyr.com>
2012-10-25 23:41:36 -0700

    Add get-metric, add tests, project cleanup.
    Adds a test to verify that integration with the librato API works
    correctly. Submits a gauge measurement and checks that it's there.
    Introduces (get-metric), which gets a metric by name.
    
    The test will prompt you to set two environment variables for your
    metrics username and API key.
    
    Also establishes shared functions for creating API URIs, converting maps
    to and from clojure-style keywords, and building HTTP requests.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-librato "0.0.1"
+(defproject clj-librato "0.0.2-SNAPSHOT"
   :description "Clojure interface to the Librato service"
   :dependencies [[org.clojure/clojure "1.2.1"]
-				 [clj-http "0.1.3"]
+                 [clj-http "0.1.3"]
                  [clj-json "0.4.0"]])
--- src/clj_librato/metrics.clj
+++ src/clj_librato/metrics.clj
@@ -1,11 +1,64 @@
 (ns clj-librato.metrics
-	(:require [clj-json.core :as json]
-		  [clj-http.client :as client]))
+  (:require [clj-json.core   :as json]
+            [clj-http.client :as client]
+            [clojure.string  :as string]
+            clj-http.util))
+
+(def uri-base "https://metrics-api.librato.com/v1/")
+
+(defn uri
+  "The full URI of a particular resource, by path fragments."
+  [& path-fragments]
+  (apply str uri-base 
+         (interpose "/" (map clj-http.util/url-encode path-fragments))))
+
+(defn unparse-kw
+  "Convert a clojure-style dashed keyword map into string underscores.
+  Recursive."
+  [m]
+  (cond
+    (map? m) (into {} (map (fn [[k v]] 
+                             [(string/replace (name k) "-" "_") 
+                              (unparse-kw v)]) 
+                           m))
+    (coll? m) (map unparse-kw m)
+    true m))
+
+(defn parse-kw
+  "Parse a response map into dashed, keyword keys. Not recursive: some librato
+  API functions return arbitrary string keys in maps."
+  [m]
+  (into {} (map (fn [[k v]] [(keyword (string/replace k "_" "-")) v]) m)))
+
+(defn request
+  "Constructs the HTTP client request map."
+  ([user api-key params]
+   {:basic-auth [user api-key]
+    :content-type :json
+    :accept :json
+    :query-params (unparse-kw params)
+    :throw-exceptions false})
+   
+  ([user api-key params body]
+   (assoc (request user api-key params)
+          :body (json/generate-string (unparse-kw body)))))
 
 (defn collate [user api-key gauges counters]
-	(client/post "https://metrics-api.librato.com/v1/metrics.json"
-		{:basic-auth [user api-key]
-		 :body (json/generate-string {:gauges gauges :counters counters})
-		 :content-type :json
-		 :accept :json
-		 :throw-exceptions false}))
+  "Posts a set of gauges and counters."
+  (client/post (uri "metrics")
+               (request user api-key {} {:gauges gauges :counters counters})))
+
+(defn get-metric
+  "Gets a metric by name."
+  ([user api-key name]
+   (get-metric user api-key name {}))
+  ([user api-key name params]
+   (let [body (-> (client/get (uri "metrics" name)
+                              (request user api-key params))
+                :body
+                json/parse-string
+                parse-kw)]
+     (assoc body :measurements
+            (into {} (map (fn [[source measurements]]
+                            [source (map parse-kw measurements)])
+                          (:measurements body)))))))
